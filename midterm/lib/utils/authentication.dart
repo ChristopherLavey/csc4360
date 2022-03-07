2e6213fdb2b7699745869720b222f7b8d27e187f
Christopher Lavey <clavey1@student.gsu.edu>
2022-03-07 16:36:20 -0500

    finished login

--- midterm/lib/utils/authentication.dart
+++ midterm/lib/utils/authentication.dart
@@ -4,11 +4,25 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+import '../screens/user_info_screen.dart';
+
 class Authentication {
-  static Future<FirebaseApp> initializeFirebase() async {
+  static Future<FirebaseApp> initializeFirebase({
+    required BuildContext context,
+  }) async {
     FirebaseApp firebaseApp = await Firebase.initializeApp();
 
-    // To-Do: Add auto login logic
+    User? user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => UserInfoScreen(
+            user: user,
+          ),
+        ),
+      );
+    }
 
     return firebaseApp;
   }
