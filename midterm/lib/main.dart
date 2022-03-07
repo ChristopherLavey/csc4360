09847c2d78a06ec54b1efba6886262ce19f774c4
Christopher Lavey <clavey1@student.gsu.edu>
2022-03-07 07:20:36 -0500

    setup gradle

--- midterm/lib/main.dart
+++ midterm/lib/main.dart
@@ -4,21 +4,5 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MessagingApp extends StatelessWidget {
-  //build app here
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Messaging App',
-      home: MessagingApp(),
-    );
-  }
+  return;
 }
