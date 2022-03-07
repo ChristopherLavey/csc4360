efe0c9f3117e6283920139852e41b57fc63386b5
Christopher Lavey <clavey1@student.gsu.edu>
2022-03-07 07:04:49 -0500

    updated dependecies, setup firebase project

--- midterm/lib/main.dart
+++ midterm/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 void main() {
   runApp(const MyApp());
