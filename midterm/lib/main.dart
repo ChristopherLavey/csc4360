f431f9b6986fd9e3cce0d68f8cb6a04ebf90e438
Christopher Lavey <clavey1@student.gsu.edu>
2022-03-07 14:48:56 -0500

    setup authentication

--- midterm/lib/main.dart
+++ midterm/lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
+import 'screens/sign_in_screen.dart';
 
 void main() {
-  return;
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterFire Samples',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        brightness: Brightness.dark,
+      ),
+      home: SignInScreen(),
+    );
+  }
 }
